f76b79fda3986fce84f9dae370e17207161a9286
Gokuldevx <gokuldevse2001@gmail.com>
2025-11-17 13:32:35 +0530

    Add rewards configuration for XP and levels

---
+++ data/rewards_config.py
@@ -0,0 +1,12 @@
+{
+    "xp_per_focus_minute": 2,
+    "xp_per_smile": 5,
+    "xp_per_break": 1,
+    "levels": {
+        "1": 0,
+        "2": 30,
+        "3": 70,
+        "4": 120,
+        "5": 200
+    }
+}
